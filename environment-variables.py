18319b65be24f402a8141151b21e10df34f6a992
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2015-09-05 20:45:36 +0200

    Print environment variables in text/plain format

--- environment-variables.py
+++ environment-variables.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
+import os
 app = Flask(__name__)
 
 @app.route('/')
-def envVariables():
+def env_var():
+	env_var_dict = os.environ
+	env_var_str = ""
+
+	for k, v in env_var_dict.iteritems():
+		env_var_str += str(k) + ": " + str(v) + "\n"
+
+	return Response(env_var_str, mimetype='text/plain')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
